da50eb7b3f6e9761cc4900bd6490832f8cac88d4
Sam(Gangmin) Bae <sbae000420@gmail.com>
2021-11-03 09:20:09 -0700

    created firebase and modified html files.

---
+++ scripts/firebaseBBY07.js
@@ -0,0 +1,12 @@
+var  firebaseConfig = {
+  apiKey: "AIzaSyB4Qn1SNrxqVa6fOMMUXiVN5LqzvLOLVTY",
+  authDomain: "comp1800---bby07.firebaseapp.com",
+  projectId: "comp1800---bby07",
+  storageBucket: "comp1800---bby07.appspot.com",
+  messagingSenderId: "865968268069",
+  appId: "1:865968268069:web:98a912ef5c2b83a81930cc",
+  measurementId: "G-826FYTCV35"
+  };
+  
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
