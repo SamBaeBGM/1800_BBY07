e45e526f82c8b232f2b3a182eb8c6d1d5f2accb2
Sam(Gangmin) Bae <sbae000420@gmail.com>
2021-11-10 08:59:14 -0800

    few changes

--- scripts/remindercreation.js
+++ scripts/remindercreation.js
@@ -4,7 +4,7 @@ const important = document.getElementById("important");
 function addReminder(remind) {
   const text = <li class="reminder">
     <i class="form-check-input me-1" type="checkbox" value="Reminder1" job="complete"></i>
-    <p class="text"> ${remind} </p>
+    <p class="text"> </p>
     </li>
 
     const reminderPosition = "beforeend";
