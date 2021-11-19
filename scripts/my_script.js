fd2e2f92fe01e8d1141d5bc18a6da9d01419ab30
Shayan <shayan.zahedana@gmail.com>
2021-11-19 12:48:41 -0800

    fixed some stuff

--- scripts/my_script.js
+++ scripts/my_script.js
@@ -114,11 +114,63 @@ btn.onclick = function () {
     console.log(selectedValue)
 };
 
-var matches = element.getElementsByClassName('colorbox');
 
-for (var i=0; i<matches.length; i++) {
-  matches[i].classList.remove('colorbox');
-  matches.item(i).classList.add('hueframe');
+var currentUser
+function populateInfo() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var userName = userDoc.data().name;
+                    var userWork = userDoc.data().work;
+                    var userEmail = userDoc.data().email;
+
+                    //if the data fields are not empty, then write them in to the form.
+                    if (userName != null) {
+                        document.getElementById("nameInput").value = userName;
+                    }
+                    if (userWork != null) {
+                        document.getElementById("workInput").value = userWork;
+                    }
+                    if (userEmail != null) {
+                        document.getElementById("emailInput").value = userEmail;
+                    }
+                })
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+        }
+    });
+}
+
+//call the function to run it 
+populateInfo();
+
+function editUserInfo() {
+    //Enable the form fields
+    document.getElementById('personalInfoFields').disabled = false;
+}
+
+function saveUserInfo() {
+    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+    userWork = document.getElementById('workInput').value;     //get the value of the field with id="schoolInput"
+    userEmail = document.getElementById('emailInput').value;       //get the value of the field with id="cityInput"
+
+    currentUser.update({
+        name: userName,
+        work: userWork,
+        email: userEmail
+    })
+        .then(() => {
+            console.log("Document successfully updated!");
+        })
+    document.getElementById('personalInfoFields').disabled = true;
 }
 // function myFunction() {
 
